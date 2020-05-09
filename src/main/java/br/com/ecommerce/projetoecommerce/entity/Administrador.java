c2ae921d1b428c16d7f9767979ae8ff14510f6e5
danilolimaext <danilo.lima@mcs-it.com.br>
2020-05-09 11:51:32 -0300

    Classes Pedro

--- src/main/java/br/com/ecommerce/projetoecommerce/entity/Administrador.java
+++ src/main/java/br/com/ecommerce/projetoecommerce/entity/Administrador.java
@@ -1,5 +1,60 @@
 package br.com.ecommerce.projetoecommerce.entity;
 
+import java.time.LocalDateTime;
+
 public class Administrador {
+	
+	private int codigo;
+	private String nome;
+	private LocalDateTime dataNascimento;
+	private String cpf;
+	private String rg;
+	
+	public Administrador() {
+	
+	}
+
+	public Administrador(int codigo, String nome, LocalDateTime dataNascimento, String cpf, String rg) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.cpf = cpf;
+		this.rg = rg;
+	}
+	
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDateTime getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDateTime dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	
+	
 
 }
--- src/main/java/br/com/ecommerce/projetoecommerce/entity/Produto.java
+++ src/main/java/br/com/ecommerce/projetoecommerce/entity/Produto.java
@@ -1,5 +1,83 @@
 package br.com.ecommerce.projetoecommerce.entity;
 
 public class Produto {
+	
+	private int codigo;
+	private String nome;
+	private float preço;
+	private String cor;
+	private String marca;
+	private String descricao;
+	private int estoque;
+	private int codigoAdministrador;
+	
+	public Produto() {
+		
+	}
+
+	
+	public Produto(int codigo, String nome, float preço, String cor, String marca, String descricao, int estoque,
+			int codigoAdministrador) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.preço = preço;
+		this.cor = cor;
+		this.marca = marca;
+		this.descricao = descricao;
+		this.estoque = estoque;
+		this.codigoAdministrador = codigoAdministrador;
+	}
+	
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getPreço() {
+		return preço;
+	}
+	public void setPreço(float preço) {
+		this.preço = preço;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getEstoque() {
+		return estoque;
+	}
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+	public int getCodigoAdministrador() {
+		return codigoAdministrador;
+	}
+	public void setCodigoAdministrador(int codigoAdministrador) {
+		this.codigoAdministrador = codigoAdministrador;
+	}
+	
 
 }
