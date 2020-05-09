f0761a363bd61bb8a7a05421db27dd91d11b7c5f
danilolimaext <danilo.lima@mcs-it.com.br>
2020-05-09 11:50:27 -0300

    Classes vinícius e conection

--- src/main/java/br/com/ecommerce/projetoecommerce/entity/Endereco.java
+++ src/main/java/br/com/ecommerce/projetoecommerce/entity/Endereco.java
@@ -1,5 +1,72 @@
 package br.com.ecommerce.projetoecommerce.entity;
 
 public class Endereco {
-
+	private int codigo;
+	private String logradouro;
+	private int numero;
+	private String complemento;
+	private String cep;
+	private String bairro;
+	private String cidade;
+	
+	public Endereco() {
+		
+	}
+	
+	public Endereco(int codigo, String logradouro, int numero, String complemento, String cep, String bairro,
+			String cidade) {
+		super();
+		this.codigo = codigo;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.cep = cep;
+		this.bairro = bairro;
+		this.cidade = cidade;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	
 }
--- src/main/java/br/com/ecommerce/projetoecommerce/entity/Login.java
+++ src/main/java/br/com/ecommerce/projetoecommerce/entity/Login.java
@@ -2,4 +2,36 @@ package br.com.ecommerce.projetoecommerce.entity;
 
 public class Login {
 
+	private int codigo;
+	private String usuario;
+	private String senha;
+	
+	public Login() {
+		
+	}
+	
+	public Login(int codigo, String usuario, String senha) {
+		super();
+		this.codigo = codigo;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int id_login) {
+		this.codigo = id_login;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}	
 }
