d2b1d543963ae276ab27837775a3c0ec898597c2
danilolimaext <danilo.lima@mcs-it.com.br>
2020-05-09 11:54:15 -0300

    Criação das classes Cliente e NotaCompra

--- src/main/java/br/com/ecommerce/projetoecommerce/entity/Cliente.java
+++ src/main/java/br/com/ecommerce/projetoecommerce/entity/Cliente.java
@@ -1,5 +1,106 @@
 package br.com.ecommerce.projetoecommerce.entity;
 
-public class Cliente {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+public class Cliente {
+	
+	private int codigo;
+	
+	private String nome;
+	
+	private String email;
+	
+	private LocalDateTime dataNascimento;
+	
+	private String cpf;
+	
+	private String rg;
+	
+	private Long numeroCatao;
+	
+	private Endereco endereco;
+
+	
+	 public Cliente() {
+	}
+	
+	public Cliente(int codigo, String nome, String email, LocalDateTime dataNascimento, String cpf, String rg,
+			Long numeroCatao, Endereco endereco) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.numeroCatao = numeroCatao;
+		this.endereco = endereco;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDateTime getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDateTime dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public Long getNumeroCatao() {
+		return numeroCatao;
+	}
+
+	public void setNumeroCatao(Long numeroCatao) {
+		this.numeroCatao = numeroCatao;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
 }
--- src/main/java/br/com/ecommerce/projetoecommerce/entity/NotaCompra.java
+++ src/main/java/br/com/ecommerce/projetoecommerce/entity/NotaCompra.java
@@ -1,5 +1,104 @@
 package br.com.ecommerce.projetoecommerce.entity;
 
-public class NotaCompra {
+import java.time.LocalDateTime;
 
+public class NotaCompra {
+	
+	private int codigo;
+	
+	private LocalDateTime garantiaPadrao;
+	
+	private LocalDateTime garantiaExtendida;
+	
+	private float valorTotal;
+	
+	private LocalDateTime dataCompra;
+	
+	private LocalDateTime dataEntrega;
+	
+	private Cliente cliente;
+	
+	private Produto produto;
+	
+	public NotaCompra() {
+	}
+
+	public NotaCompra(int codigo, LocalDateTime garantiaPadrao, LocalDateTime garantiaExtendida, float valorTotal,
+			LocalDateTime dataCompra, LocalDateTime dataEntrega, Cliente cliente, Produto produto) {
+		super();
+		this.codigo = codigo;
+		this.garantiaPadrao = garantiaPadrao;
+		this.garantiaExtendida = garantiaExtendida;
+		this.valorTotal = valorTotal;
+		this.dataCompra = dataCompra;
+		this.dataEntrega = dataEntrega;
+		this.cliente = cliente;
+		this.produto = produto;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public LocalDateTime getGarantiaPadrao() {
+		return garantiaPadrao;
+	}
+
+	public void setGarantiaPadrao(LocalDateTime garantiaPadrao) {
+		this.garantiaPadrao = garantiaPadrao;
+	}
+
+	public LocalDateTime getGarantiaExtendida() {
+		return garantiaExtendida;
+	}
+
+	public void setGarantiaExtendida(LocalDateTime garantiaExtendida) {
+		this.garantiaExtendida = garantiaExtendida;
+	}
+
+	public float getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(float valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public LocalDateTime getDataCompra() {
+		return dataCompra;
+	}
+
+	public void setDataCompra(LocalDateTime dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+
+	public LocalDateTime getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(LocalDateTime dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	
 }
